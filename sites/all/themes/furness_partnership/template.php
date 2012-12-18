41426c7885788707217805c343625cceb6becc0e
Hasan Ozduru <hasozduru@yahoo.com>
2012-12-18 03:38:43 -0430

    furness_partnership_preprocess_html added.

--- sites/all/themes/furness_partnership/template.php
+++ sites/all/themes/furness_partnership/template.php
@@ -10,3 +10,25 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+
+/**
+ * Preprocesses the wrapping HTML.
+ *
+ * @param array &$variables
+ *   Template variables.
+ */
+function furness_partnership_preprocess_html(&$vars) {
+  // Setup IE meta tag to force IE rendering mode
+  $meta_ie_render_engine = array(
+    '#type' => 'html_tag',
+    '#tag' => 'meta',
+    '#attributes' => array(
+      'http-equiv' => 'X-UA-Compatible',
+      'content' =>  'IE=edge,chrome=1',
+    ),
+    '#weight' => '-99999',
+  );
+
+  // Add header meta tag for IE to head
+  drupal_add_html_head($meta_ie_render_engine, 'meta_ie_render_engine');
+}
